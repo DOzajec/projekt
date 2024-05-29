0bd2c27c90c6967c8b23d4d71622440d120b75f0
DOzajec <dsw51705@student.dsw.edu.pl>
2024-05-29 09:58:36 +0200

    Wczytanie json i weryfikacja tego pliku

--- zamiana.py
+++ zamiana.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+import json
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Konwersja danych między formatami .xml, .json i .yml.")
@@ -13,3 +14,16 @@ if __name__ == "__main__":
     print(f"Plik wejściowy: {input_file}")
     print(f"Plik wyjściowy: {output_file}")
 
+def load_json(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        print("Plik JSON został poprawnie wczytany.")
+        return data
+    except json.JSONDecodeError as e:
+        print(f"Błąd składni JSON: {e}")
+    except FileNotFoundError:
+        print(f"Plik {file_path} nie został znaleziony.")
+    except Exception as e:
+        print(f"Nieoczekiwany błąd: {e}")
+        return None
